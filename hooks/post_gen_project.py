c561690415c852b9e4921e8d2f609d6c3695029f
Nam Ngo <emoinrp@gmail.com>
2016-01-14 02:10:49 +1100

    fire up containers

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,28 @@
 import subprocess
 
-commands = [
-    'docker-machine', 'create', '--driver',
-    'virtualbox', '{{cookiecutter.repo_name}}'
-]
 
-process = subprocess.Popen(
-    commands, stdout=subprocess.PIPE
-)
-process.communicate()
+def create_docker_machine():
+    commands = [
+        'docker-machine', 'create', '--driver',
+        'virtualbox', '{{cookiecutter.repo_name}}'
+    ]
+    print 'Creating a new docker-machine: {{cookiecutter.repo_name}}...'
+    process = subprocess.Popen(
+        commands, stdout=subprocess.PIPE
+    )
+    process.communicate()
+
+
+def docker_compose_up():
+    commands = [
+        'docker-compose', 'up', '-d',
+    ]
+    print 'Firing up new "{{cookiecutter.repo_name}}" containers...'
+    process = subprocess.Popen(
+        commands, stdout=subprocess.PIPE
+    )
+    process.communicate()
+
+
+create_docker_machine()
+docker_compose_up()

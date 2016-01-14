43cb5b5dd71b138b22822a3537f32292c40a7774
Nam Ngo <emoinrp@gmail.com>
2016-01-14 14:12:07 +1100

    Refactor subprocess calls.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,30 +3,43 @@ import webbrowser
 import os
 
 
+class CommandLine(object):
+
+    def __init__(self, command):
+        self.command = command
+        self.command_split = command.split(' ')
+
+    def run(self, with_envs=True):
+        if with_envs:
+            process = subprocess.Popen(
+                self.command_split, stdout=subprocess.PIPE,
+            )
+        else:
+            process = subprocess.Popen(
+                self.command_split, stdout=subprocess.PIPE,
+                env=dict(os.environ)
+            )
+        return process.communicate()
+
+    def get_return(self):
+        return subprocess.check_output(
+            self.command_split
+        ).split('\n')[0]
+
+
 def create_docker_machine():
-    commands = [
-        'docker-machine', 'create', '--driver',
-        'virtualbox', '{{cookiecutter.repo_name}}'
-    ]
+
     print 'Creating a new docker-machine: {{cookiecutter.repo_name}}...'
-    process = subprocess.Popen(
-        commands, stdout=subprocess.PIPE
-    )
-    process.communicate()
+    CommandLine(
+        'docker-machine create --driver virtualbox {{cookiecutter.repo_name}}'
+    ).run()
 
 
 def set_envs():
-
-    envs = [
-        'docker-machine', 'env', 'kutta'
-    ]
-    process = subprocess.Popen(
-        envs, stdout=subprocess.PIPE
-    )
-    res = process.communicate()[0].split('\n')
+    output = CommandLine('docker-machine env {{cookiecutter.repo_name}}').run()
     output = [
         line
-        for line in res
+        for line in output[0].split('\n')
         if line.startswith('export')
     ][:-1]
 
@@ -37,25 +50,25 @@ def set_envs():
 
 
 def docker_compose_up():
-    commands = [
-        'docker-compose', 'up', '-d',
-    ]
     print 'Firing up new "{{cookiecutter.repo_name}}" containers...'
-    process = subprocess.Popen(
-        commands, stdout=subprocess.PIPE,
-        env=dict(os.environ)
-    )
-    process.communicate()
+    CommandLine('docker-compose up -d').run()
 
 
-def open_browser():
-    ip = subprocess.check_output(
-        ['docker-machine', 'ip', '{{cookiecutter.repo_name}}']
-    ).split('\n')[0]
-    webbrowser.open('http://%s' % ip)
+def get_ip():
+    return CommandLine(
+        'docker-machine ip {{cookiecutter.repo_name}}'
+    ).get_return()
+
+
+def append_host_file(ip):
+    CommandLine(
+        'sudo echo "%s {{cookiecutter.repo_name}}.dev" >> /etc/hosts' % ip
+    ).run()
 
 
 create_docker_machine()
 set_envs()
 docker_compose_up()
-open_browser()
+ip = get_ip()
+append_host_file(ip)
+webbrowser.open('http://{{cookiecutter.repo_name}}.dev')
